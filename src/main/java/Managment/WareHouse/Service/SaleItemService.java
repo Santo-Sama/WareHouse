1c621afbf05ac00635fe478b8143e5a333a6006e
Santo La Rocca <laroccasantoandrea@gmail.com>
2025-05-31 22:59:50 +0200

    created SalesService and SaleItemService

--- src/main/java/Managment/WareHouse/Service/SaleItemService.java
+++ src/main/java/Managment/WareHouse/Service/SaleItemService.java
@@ -1,4 +1,71 @@
 package Managment.WareHouse.Service;
 
+import Managment.WareHouse.DTO.SaleItemDTO;
+import Managment.WareHouse.Entity.SaleItem;
+import Managment.WareHouse.Repository.SaleItemRepository;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SaleItemService {
+
+    @Autowired
+    private final SaleItemRepository saleItemRepository;
+    private final GenericUtils genericUtils;
+    private final ModelMapper modelMapper;
+
+    public SaleItemService(SaleItemRepository saleItemRepository,ModelMapper  modelMapper  , GenericUtils genericUtils) {
+        this.genericUtils = genericUtils;
+        this.modelMapper = modelMapper;
+        this.saleItemRepository = saleItemRepository;
+    }
+
+    @Transactional
+    public ResponseEntity<?> saveSaleItem(SaleItemDTO saleItemDTO) {
+        if (genericUtils.productExists(saleItemDTO.getProduct_id())) {
+            if (genericUtils.saleExists(saleItemDTO.getSale_id())) {
+                SaleItem saleItem = modelMapper.map(saleItemDTO, SaleItem.class);
+
+                saleItem.setSale(genericUtils.getSale(saleItemDTO.getSale_id()));
+                saleItem.setProduct(genericUtils.getProduct(saleItemDTO.getProduct_id()));
+
+                SaleItem saleItemSaved = saleItemRepository.save(saleItem);
+
+                return ResponseEntity.ok(modelMapper.map(saleItemSaved, SaleItemDTO.class));
+            }else {
+                return ResponseEntity.badRequest().body("Sale does not exist");
+            }
+        }else {
+            return ResponseEntity.badRequest().body("Product does not exist");
+        }
+    }
+
+    @Transactional
+    public ResponseEntity<?> deleteSaleItem(Long id) {
+        if (genericUtils.saleItemExists(id)) {
+            saleItemRepository.deleteById(id);
+            return ResponseEntity.ok().body("SaleItem deleted successfully");
+        }else {
+            return ResponseEntity.badRequest().body("SaleItem does not exist");
+        }
+    }
+
+    public List<SaleItemDTO> getAllSaleItems() {
+        return saleItemRepository.findAll().stream()
+                .map(saleItem -> modelMapper.map(saleItem, SaleItemDTO.class)).toList();
+    }
+
+    public ResponseEntity<?> getSaleItem(Long id) {
+        if (genericUtils.saleItemExists(id)) {
+            SaleItem saleItem = saleItemRepository.findById(id).get();
+            return ResponseEntity.ok(modelMapper.map(saleItem, SaleItemDTO.class));
+        }else {
+            return ResponseEntity.badRequest().body("SaleItem does not exist");
+        }
+    }
 }
--- src/main/java/Managment/WareHouse/Service/SalesService.java
+++ src/main/java/Managment/WareHouse/Service/SalesService.java
@@ -1,4 +1,64 @@
 package Managment.WareHouse.Service;
 
+
+import Managment.WareHouse.DTO.SalesDTO;
+import Managment.WareHouse.Entity.Sales;
+import Managment.WareHouse.Repository.SalesRepository;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class SalesService {
+
+    @Autowired
+    private final SalesRepository salesRepository;
+    private final GenericUtils genericUtils;
+    private final ModelMapper modelMapper;
+
+    public SalesService(SalesRepository salesRepository, GenericUtils genericUtils, ModelMapper modelMapper) {
+        this.salesRepository = salesRepository;
+        this.genericUtils = genericUtils;
+        this.modelMapper = modelMapper;
+    }
+
+    @Transactional
+    public ResponseEntity<?> saveSale(SalesDTO salesDTO) {
+        Sales sales = modelMapper.map(salesDTO, Sales.class);
+        Sales savedSales = salesRepository.save(sales);
+        return ResponseEntity.ok(modelMapper.map(savedSales, SalesDTO.class));
+    }
+
+    @Transactional
+    public ResponseEntity<?> deleteSale(Long id) {
+        if (genericUtils.saleExists(id)) {
+            salesRepository.deleteById(id);
+            return ResponseEntity.ok().body("Sale deleted successfully");
+        } else {
+            return ResponseEntity.badRequest().body("Sale does not exist");
+        }
+    }
+
+    public ResponseEntity<SalesDTO> getSale(Long id) {
+
+        //always present bc is the current sale
+        Optional<Sales> sales = salesRepository.findById(id);
+
+        SalesDTO salesDTO = modelMapper.map(sales.get(), SalesDTO.class);
+        salesDTO.setSaleItemsDTO(genericUtils.getSaleItemDTOForSale(id));
+        return ResponseEntity.ok(salesDTO);
+
+    }
+
+    public List<SalesDTO> getAllSales() {
+
+        return salesRepository.findAll().stream()
+                .map(sales -> modelMapper.map(sales, SalesDTO.class)).toList();
+    }
+
 }
