25caf561949d697ec8f4a24e461fd21042eafbba
Santo La Rocca <laroccasantoandrea@gmail.com>
2025-05-31 22:36:38 +0200

    Added function getbysaleid for have a list of saleitem for the sale

--- src/main/java/Managment/WareHouse/Repository/SaleItemRepository.java
+++ src/main/java/Managment/WareHouse/Repository/SaleItemRepository.java
@@ -3,5 +3,8 @@ package Managment.WareHouse.Repository;
 import Managment.WareHouse.Entity.SaleItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SaleItemRepository extends JpaRepository<SaleItem, Long> {
+    List<SaleItem> findBySaleId(Long saleId);
 }
--- src/main/java/Managment/WareHouse/Repository/SalesRepository.java
+++ src/main/java/Managment/WareHouse/Repository/SalesRepository.java
@@ -3,5 +3,5 @@ package Managment.WareHouse.Repository;
 import Managment.WareHouse.Entity.Sales;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SalesRepository extends JpaRepository<Sales, Integer> {
+public interface SalesRepository extends JpaRepository<Sales, Long> {
 }
